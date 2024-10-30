aaa5970eeaa35c067e2c93fa59d2011b737fcca4
STEVAO SEVERO RODRIGUES <04460096013@senacrs.edu.br>
2024-10-30 20:29:00 -0300

    div
    de criaaaa

---
+++ cal.ts
@@ -0,0 +1,20 @@
+export class Calculadora {
+    numer01 : number;
+    numer02: number;
+
+    constructor(numer01: number, numer02: number){
+        this.numer01 = numer01;
+        this.numer02 = numer02;
+    }
+
+    Soma(): void {
+        let soma = this.numer01 + this.numer02
+        console.log(soma);
+    }
+    Sub(): void {
+        let sub = this.numer01 - this.numer02
+        console.log(sub);
+    }
+
+    
+}
