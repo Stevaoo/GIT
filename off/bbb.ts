578d3adbf767ce858e8667188cb567fe089fd54e
STEVAO SEVERO RODRIGUES <04460096013@senacrs.edu.br>
2024-10-30 21:16:45 -0300

    Terceiro commit
    boraaaaaaaa

--- off/bbb.ts
+++ off/bbb.ts
@@ -16,5 +16,11 @@ export class Calculadora {
         let sub = this.numer01 - this.numer02
         console.log(sub);
     }
-    //
+    
+    
+    div(): void {
+        let div = this.numer01 / this.numer02
+        console.log(div);
+    }
+
 }
